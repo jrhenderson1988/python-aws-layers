0ebd9ddbd2ae0628174a1eedf3a1d1ab8bb3aa3c
Jonathon Henderson <jrhenderson1988@hotmail.com>
2020-02-11 15:50:30 +0000

    Fixed folder name

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 mkdir -p layer/python3.8/site-packages
 pipenv run pip freeze > layer/python3.8/requirements.txt
-cp -r python/common layer/python3.8/site-packages
+cp -r project/common layer/python3.8/site-packages
